4e9fc9f747680946bfa9c064706aab51bcd2be97
Ilko García <ilko.garcia@gmail.com>
2023-05-08 20:21:30 +0200

    feat: added a link in petCard component that link with the detail view page

--- src/components/pet-card.jsx
+++ src/components/pet-card.jsx
@@ -4,10 +4,11 @@
 import { useEffect, useRef } from 'react'
 import { PropTypes } from 'prop-types'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function PetCard({ pet, newLimit, isLast }) {
   const { PetBreed, name, age, imageUrl, description } = pet
-  const  { breedName, PetSpecie } = PetBreed
+  const { breedName, PetSpecie } = PetBreed
   const { specieCommonName } = PetSpecie
 
   const cardRef = useRef()
@@ -25,7 +26,10 @@ function PetCard({ pet, newLimit, isLast }) {
   }, [isLast])
 
   return (
-    <div className='flex h-full w-full flex-col rounded-md bg-white shadow-md' ref={cardRef}>
+    <div
+      className='flex h-full w-full flex-col rounded-md bg-white shadow-md'
+      ref={cardRef}
+    >
       <div className='relative h-80 w-full overflow-hidden rounded-t-md'>
         <Image
           alt={name + ' ' + specieCommonName + ' ' + breedName}
@@ -39,26 +43,32 @@ function PetCard({ pet, newLimit, isLast }) {
           }}
         />
       </div>
-      <div className='w-full px-6 py-4'>
-        <div className='text-md mb-2 space-y-2 font-bold text-gray-400 text-left'>
-          <p className='text-2xl font-extrabold text-gray-600'>
-            {name}{' '}
-            <span className='text-sm font-light text-gray-400'>({age} years old)</span>
-            <span className='block align-baseline text-sm font-bold text-green-400 uppercase'>
-              {breedName + ' / ' + specieCommonName}
-            </span>
-          </p>
-          <p className='text-lg text-gray-400 font-extralight'>
-            {description}
-          </p>
-        </div>
+      <div className='mb-2 flex h-auto w-full flex-col space-y-2 px-6 py-4 text-left font-bold text-gray-400'>
+        <p className='text-2xl font-extrabold text-gray-600'>
+          {name}{' '}
+          <span className='text-sm font-light text-gray-400'>
+            ({age} years old)
+          </span>
+          <span className='block align-baseline text-sm font-bold uppercase text-green-400'>
+            {breedName + ' / ' + specieCommonName}
+          </span>
+        </p>
+        <p className='text-lg font-extralight text-gray-400'>{description}</p>
+      </div>
+      <div className='mb-4 mt-auto px-6 flex h-auto w-full text-left'>
+        <Link
+          className='text-sm text-green-400 hover:text-green-700 active:text-green-900'
+          href={`/pets/${pet.id}`}
+        >
+          View more...
+        </Link>
       </div>
     </div>
   )
 }
 
 PetCard.propTypes = {
-  pet: PropTypes.array,
+  pet: PropTypes.object,
   newLimit: PropTypes.func,
   isLast: PropTypes.bool,
 }
