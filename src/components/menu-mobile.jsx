77cc8e20caf32b9bb9c7f32f38ea8a25d7992162
Ilko García <ilko.garcia@gmail.com>
2023-05-11 22:48:20 +0200

    feat: added new menu options to the mobile menu. When user is loged in, now will show links to the restricted access sections

--- src/components/menu-mobile.jsx
+++ src/components/menu-mobile.jsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import { HiXMark } from 'react-icons/hi2'
 import { useEffect, useState } from 'react'
+import { signOut, useSession } from 'next-auth/react'
 
 const mainMenu = [
   {
@@ -27,8 +28,36 @@ const mainMenu = [
   },
 ]
 
+const userMenu = [
+  {
+    label: 'Dashboard',
+    route: '/user',
+  },
+  {
+    label: 'My Applications',
+    route: '/user',
+  },
+  {
+    label: 'My Favorites',
+    route: '/user',
+  },
+  {
+    label: 'My Messages',
+    route: '/user',
+  },
+  {
+    label: 'My Donations',
+    route: '/user',
+  },
+  {
+    label: 'My Account',
+    route: '/user/account',
+  },
+]
+
 export default function MobileMenu({ close }) {
   const [animation, setAnimation] = useState(false)
+  const { data: session } = useSession()
 
   useEffect(() => {
     setAnimation(true)
@@ -43,23 +72,24 @@ export default function MobileMenu({ close }) {
   }, [])
 
   return (
-    <>
-      <div className='absolute inset-0 top-0 z-10 h-screen w-full bg-gray-500 bg-opacity-60 backdrop-blur-sm '>
-        <div className='inset-0 top-0 p-5'>
-          <div
-            className={`w-full rounded-xl bg-white p-5 transition-all  ${
-              animation ? 'scale-100' : 'scale-90'
-            }`}
-          >
-            <div className='flex items-center justify-between'>
-              <h1>Navigation</h1>
-              <HiXMark
-                size={25}
-                className='h-7 w-7 cursor-pointer transition-all hover:scale-110'
-                onClick={close}
-              />
-            </div>
-            <div className='mt-5 divide-y'>
+    <div className='absolute inset-0 top-0 z-10 h-screen w-full bg-gray-500 bg-opacity-60 backdrop-blur-sm '>
+      <div className='inset-0 top-0 p-5'>
+        <div
+          className={`w-full rounded-xl bg-white p-5 transition-all  ${
+            animation ? 'scale-100' : 'scale-90'
+          }`}
+        >
+          <div className='flex items-center justify-between'>
+            <h1>Navigation</h1>
+            <HiXMark
+              size={25}
+              className='h-7 w-7 cursor-pointer transition-all hover:scale-110'
+              onClick={close}
+            />
+          </div>
+
+          <div className='mt-5 grid grid-cols-2'>
+            <div className='col-span-1'>
               <ul className='list-none gap-2 md:gap-4 lg:gap-8'>
                 {mainMenu.map(({ label, route }) => (
                   <li key={route} className='block px-4 py-2'>
@@ -73,9 +103,58 @@ export default function MobileMenu({ close }) {
                 ))}
               </ul>
             </div>
+
+            <div className='col-span-1'>
+              {session?.user ? (
+                <ul className='list-none gap-2 md:gap-4 lg:gap-8'>
+                  {userMenu.map(({ label, route }) => (
+                    <li key={route} className='block px-4 py-2'>
+                      <Link
+                        className='text-sm text-green-600 hover:text-green-800 hover:underline hover:decoration-solid hover:decoration-2 hover:underline-offset-4'
+                        href={route}
+                      >
+                        {label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <ul className='list-none gap-2 md:gap-4 lg:gap-8'>
+                  <li key='/auth/login' className='block px-4 py-2'>
+                    <Link
+                      className='text-sm text-green-600 hover:text-green-800 hover:underline hover:decoration-solid hover:decoration-2 hover:underline-offset-4'
+                      href='/auth/login'
+                    >
+                      Log in
+                    </Link>
+                  </li>
+                  <li key='/auth/register' className='block px-4 py-2'>
+                    <Link
+                      className='text-sm text-green-600 hover:text-green-800 hover:underline hover:decoration-solid hover:decoration-2 hover:underline-offset-4'
+                      href='/auth/register'
+                    >
+                      Register
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+
+            {session?.user ? (
+              <div className='flex items-center justify-between'>
+                <button
+                  className='rounded-md bg-green-600 px-3 py-2 text-green-300 shadow-sm transition duration-300 ease-in-out hover:bg-green-300 hover:text-green-600'
+                  onClick={() => signOut()}
+                >
+                  Log out
+                </button>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
