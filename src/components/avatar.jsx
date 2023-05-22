1262fc4df3c86c5d2c51ba80bc6fc3a88fee4346
Ilko García <ilko.garcia@gmail.com>
2023-05-22 19:15:22 +0200

    refactor: migrated some elements inside the avantar component. Now the Avatar component is a Link and has inside not only the image but also the username

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,16 +1,33 @@
+'use client'
+import { useSession } from 'next-auth/react'
 import Image from 'next/image'
+import Link from 'next/link'
+import { HiOutlineUserCircle } from 'react-icons/hi2'
 
-const Avatar = ({ src, alt, size, ...props }) => {
+const Avatar = () => {
+  const { data: session } = useSession()
+
   return (
-    <div className='flex items-center'>
-      <Image
-        src={src}
-        alt={alt}
-        width={size}
-        height={size}
-        {...props}
-      />
-    </div>
+    <>
+      <Link href='/user' className='flex items-center space-x-2'>
+        {session?.user?.image ? (
+          <Image
+            src={session.user.image}
+            alt={session.user.username}
+            width={32}
+            height={32}
+            className='rounded-full border border-gray-400'
+          />
+        ) : (
+          <span className='icon text-gray-600'>
+            <HiOutlineUserCircle size={32} />
+          </span>
+        )}
+        <span className='icon items-center text-gray-600'>
+          {session.user.username || 'Unknow'}
+        </span>
+      </Link>
+    </>
   )
 }
 
--- src/components/menu-top-user.jsx
+++ src/components/menu-top-user.jsx
@@ -1,44 +1,25 @@
 'use client'
 import Link from 'next/link'
-import { HiOutlineUserCircle } from 'react-icons/hi2'
 import { signOut, useSession } from 'next-auth/react'
 import Avatar from './avatar'
 
-export default function UserMenu() {
+const UserMenu = () => {
   const { data: session } = useSession()
 
   return (
     <div className=' mx-auto flex h-fit w-fit items-center justify-between gap-2 md:gap-4 lg:gap-8'>
-      {session?.user ? (
-        <div className='flex items-center justify-between space-x-2'>
-          {session.user.image ? (
-            <Avatar
-              src={session.user.image}
-              alt={session.user.username}
-              size={30}
-              className='rounded-full border border-gray-400'
-            />
-          ) : (
-            <span className='icon flex items-center px-2'>
-              <HiOutlineUserCircle size={22} />
-            </span>
-          )}
-          <h2 className='text-gray-600'>
-            <Link href='/user'>{session.user.username || 'Unknow'}</Link>
-          </h2>
-        </div>
+      {session ? (
+        <>
+          <Avatar />
+          <button
+            className='rounded-md bg-green-600 px-3 py-2 text-green-300 shadow-sm transition duration-300 ease-in-out hover:bg-green-300 hover:text-green-600'
+            onClick={() => signOut({ callbackUrl: '/'})}
+          >
+            Log out
+          </button>
+        </>
       ) : (
-        <></>
-      )}
-      {session?.user ? (
-        <button
-          className='rounded-md bg-green-600 px-3 py-2 text-green-300 shadow-sm transition duration-300 ease-in-out hover:bg-green-300 hover:text-green-600'
-          onClick={() => signOut()}
-        >
-          Log out
-        </button>
-      ) : (
-        <div className='flex items-center justify-between gap-6'>
+        <>
           <Link
             className='text-green-900 hover:text-white hover:underline hover:decoration-solid hover:decoration-2 hover:underline-offset-4'
             href='/auth/login'
@@ -51,8 +32,10 @@ export default function UserMenu() {
           >
             Register
           </Link>
-        </div>
+        </>
       )}
     </div>
   )
 }
+
+export default UserMenu
